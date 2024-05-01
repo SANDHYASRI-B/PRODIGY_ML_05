1557a5387bbe10e15ea09af9e06641e43628891c
SANDHYASRI-B <145352364+SANDHYASRI-B@users.noreply.github.com>
2024-05-01 14:28:59 +0530

    ML5.py

---
+++ ML5.py
@@ -0,0 +1,52 @@
+!pip install streamlit
+import streamlit as st
+from PIL import Image
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+import cv2
+from google.colab.patches import cv2_imshow
+from skimage.transform import resize
+from google.colab.patches import cv2_imshow
+import pandas as pd
+
+model = load_model('FV.h5')
+
+labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
+          7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger',
+          14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
+          19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple',
+          26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn',
+          32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
+
+fruits = ['Apple', 'Banana', 'Bello Pepper', 'Chilli Pepper', 'Grapes', 'Jalepeno', 'Kiwi', 'Lemon', 'Mango', 'Orange',
+          'Paprika', 'Pear', 'Pineapple', 'Pomegranate', 'Watermelon']
+vegetables = ['Beetroot', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Corn', 'Cucumber', 'Eggplant', 'Ginger',
+              'Lettuce', 'Onion', 'Peas', 'Potato', 'Raddish', 'Soy Beans', 'Spinach', 'Sweetcorn', 'Sweetpotato',
+              'Tomato', 'Turnip']
+
+def prepare_image(img_path):
+    img = load_img(img_path, target_size=(224, 224, 3))
+    img = img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, [0])
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    return res.title()
+
+df=pd.read_csv('/content/calories.csv')
+img_file= '/content/i7.jpg'
+result = prepare_image('/content/i7.jpg')
+df1=df[(df['FoodItem']==result)]
+df1=df1.drop_duplicates('FoodItem')
+calorie=df1['Cals_per100grams'].values
+image=cv2.imread(img_file)
+
+cv2_imshow(image)
+print("Recognized food is",result)
+print("Calories of the recognized food item is" , *calorie + " per 100 grams")
